7a8e156c37713d9b55ff074f409efcb459db7390
Emma Schaffer <emmaschaffer@usf.edu>
2021-02-05 14:59:20 -0500

    Add project files.

---
+++ Method/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string userName = Console.ReadLine();
+            Hello_user(userName);
+        }
+
+        private static void Hello_user(string name)
+        {
+            Console.WriteLine("Hello" + name);
+        }
+    }
+}
